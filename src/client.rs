359f704dea29cccc96fe49bb9d4a3bafd7717f3c
p-brennan3 <p-brennan3>
2024-12-08 21:40:09 -0500

    async with multiple clients

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,3 @@
-// client.rs
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpStream,
@@ -7,26 +6,43 @@ use std::io::{self, Write};
 
 #[tokio::main]
 async fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:4999").await.unwrap();
-    let (reader, mut writer) = stream.split();
+    let stream = TcpStream::connect("127.0.0.1:4999").await.unwrap();
+    let (reader, writer) = stream.into_split();
+
+    // task for receiving messages
     let mut reader = BufReader::new(reader).lines();
-
-    println!("Connected! Type messages (Ctrl+C to quit)");
-    
-    loop {
-        print!("> ");
+    let receive_task = tokio::spawn(async move {
+        // this loop continues until the connection closes 
+      while let Some(response) = reader.next_line().await.unwrap() {
+        print!("\r");  
+        println!("{response}");  
+        print!("> ");  
         io::stdout().flush().unwrap();
-        
-         // Get input, send it on the SAME connection
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let input = input.trim();
+      }
+    });
 
-        writer.write_all(input.as_bytes()).await.unwrap();
-        writer.write_all(b"\n").await.unwrap();
+    // task for sending messages
+    let mut writer = writer;
+    let send_task = tokio::spawn(async move {
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            writer.write_all(input.trim().as_bytes()).await.unwrap();
+            writer.write_all(b"\n").await.unwrap();   
 
-        if let Some(response) = reader.next_line().await.unwrap() {
-            println!("{}", response);
+
+            // we don't want to see what we entered
+            // so ove cursor up one line and clear it
+            print!("\x1B[1A\x1B[2K");
+            io::stdout().flush().unwrap();
         }
+    });
+
+    // wait for either task to finish (like if server disconnects)
+    tokio::select! {
+        _ = receive_task => println!("Receive task ended"),
+        _ = send_task => println!("Send task ended"),
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,29 +1,66 @@
-// server.rs
 use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::{TcpListener, TcpStream},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},  
+    net::{TcpListener, TcpStream},                    
+    sync::broadcast,                                 
 };
 
 #[tokio::main]
 async fn main() {
+
     let listener = TcpListener::bind("127.0.0.1:4999").await.unwrap();
     println!("Server listening on 4999...");
 
+    // create broadcast channel - every message sent through this will go to all subscribers
+    // tx is the sender side, _rx is receiver we don't need (since each connection makes their own)
+    let (tx, _rx) = broadcast::channel(100);
+
+    // main server loop - continuously accept new connections
     loop {
+        // wait for new client to connect, socket is their connection, addr is their IP/port
         let (socket, addr) = listener.accept().await.unwrap();
         println!("New client: {}", addr);
-        tokio::spawn(handle_connection(socket));
+        
+        // clone tx so this client can send messages to channel
+        let tx = tx.clone();
+        // create new receiver so this client can receive messages from channel
+        let rx = tx.subscribe();
+        
+        // spawn new task to handle this client
+        tokio::spawn(handle_connection(socket, tx, rx, addr));
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
-    let (reader, mut writer) =  stream.split();
+// function to handle each client connection
+async fn handle_connection(
+    stream: TcpStream,
+    tx: broadcast::Sender<(String, std::net::SocketAddr)>,  // to send messages
+    mut rx: broadcast::Receiver<(String, std::net::SocketAddr)>,  // to receive messages
+    addr: std::net::SocketAddr,  // Client's address
+) {
+    // split connection into read and write parts
+    let (reader, writer) = stream.into_split();
+    // create buffered reader for reading lines
     let mut reader = BufReader::new(reader).lines();
 
-    // this loop keeps running as long as the connection is open
+    // spawn task to handle receiving broadcast messages
+    let mut writer = writer;
+    tokio::spawn(async move {
+        //  this loop runs indefinitely
+        // continuously receiving messages from broadcast channel and writing to client stream
+        while let Ok((msg, from_addr)) = rx.recv().await {
+            // Format and send message to client
+          writer.write_all(format!("{}: {}\n", from_addr, msg).as_bytes()).await.unwrap();  
+        }
+    });
+
+    // main loop for this client - read their messages and broadcast them
+    // this loop runs indefinitely until:
+    //   * the client disconnects (which would make next_line() return None)
+    //   * there's an error (which would panic due to the unwrap)
     while let Some(line) = reader.next_line().await.unwrap() {
-        println!("Got: {}", line);
-        // Echo back with a prefix
-        writer.write_all(format!("Server received: {}\n", line).as_bytes()).await.unwrap();
+        println!("Got message from {}: {}", addr, line);
+        // send message to broadcast channel along with sender's address 
+        // so that other connect clients will receive it (via the task above -> tx.recv())
+        tx.send((line, addr)).unwrap();
     }
 }
